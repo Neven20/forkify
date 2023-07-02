4aaa168f514ca005d5624293ed7543f03c5e1046
Neven20 <neven.vodopija20@gmail.com>
2023-07-02 23:30:45 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 5;
+export const KEY = '1257fb5e-0fad-4b88-a68f-ea51bec2aea2';
+export const MODAL_CLOSE_SEC = 2.5;
